cc4d6eb40ad3c852db6311cf332395590ea9605a
kh-issa86 <72387525+kh-issa86@users.noreply.github.com>
2021-07-06 15:58:11 +0200

    controllers and routes

--- Backend/routes/sauces.js
+++ Backend/routes/sauces.js
@@ -6,7 +6,7 @@ const multer = require('../middleware/multer-config');
 
 const saucesCtrl = require('../controllers/sauces');
 
-router.post('', auth, multer, saucesCtrl.createSauce);
+router.post('/', auth, multer, saucesCtrl.createSauce);
 router.get('/', auth, saucesCtrl.getAllSauces);
 router.get('/:id', auth, saucesCtrl.getOneSauce);
 router.put('/:id', auth, multer, saucesCtrl.modifySauce);
