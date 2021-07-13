d6dcbafe22547931d59e7be744e1307d8ec72a3b
kh-issa86 <72387525+kh-issa86@users.noreply.github.com>
2021-07-13 16:52:43 +0200

    .env file

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1]; // we extract the token from the Authorization header of the incoming request. It will also contain the keyword Bearer.
-    const decodedToken = jwt.verify(token, 'h&^%yut876&^76');// then we use the verify function to decode our token. If this is not valid, an error will be generated;
+    const decodedToken = jwt.verify(token, process.env.KEYTOKEN);// then we use the verify function to decode our token. If this is not valid, an error will be generated;
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {// if the request contains a user ID, we compare it to the one extracted from the token. If they are different, we generate an error;
       throw 'Invalid user ID';
