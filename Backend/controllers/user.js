338e61e9792a254fc46fec0ac716f45297c3c2ba
kh-issa86 <72387525+kh-issa86@users.noreply.github.com>
2021-07-16 17:32:02 +0200

    email address encryption in database

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,54 +1,64 @@
 // import packages
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
+const crypto = require('crypto');
 const User = require("../models/User");
 require("dotenv").config();
 
 // Fonction Signup
 
 exports.signup = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10) // we call the hash function of bcrypt in our password and ask it to "hash" the password 10 times.
-    // this is an asynchronous function that returns a Promise in which we receive the generated hash;
-    .then((hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-      });
-      user
-        .save() // we create a user and save it to the database, returning a success response on success,
-        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
-        .catch((error) => res.status(400).json({ error })); // and errors with the error code in case of failure;
-    })
-    .catch((error) => res.status(500).json({ error }));
-};
+  bcrypt.hash(req.body.password, 10) // Password hashing by Bcrypt
+      .then(
+        hash => {
+          // E-mail encryption in the database
+          key = process.env.HASH_KEY;
+          cipher = crypto.createCipher('aes192', key)
+          cipher.update(req.body.email, 'binary', 'hex')
+          encodedString = cipher.final('hex')
+          const user = new User({
+              email: encodedString,
+              password: hash
+            });
+            user.save()
+                .then(() => res.status(201).json({ message: 'Utilisateur créé' }))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+    };
 
-// Fonction de Login
+// Fonction Login
 
 exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({ error: "Utilisateur non trouvé !" });
-      }
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then((valid) => {
-          if (!valid) {
-            return res.status(401).json({ error: "Mot de passe incorrect !" });
-          }
-          res.status(200).json({
-            userId: user._id,
-            token: jwt.sign(
-              // we use jsonwebtoken's sign function to encode a new token;
-              { userId: user._id }, // this token contains the user's ID as a payload (the data encoded in the token);
-              process.env.KEYTOKEN,
-              { expiresIn: "24h" } // we set the validity period of the token to 24 hours. The user will therefore have to reconnect after 24 hours;
-            ),
-          });
-        })
-        .catch((error) => res.status(500).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+  // E-mail encryption in order to compare it with the one in the database
+  key = process.env.HASH_KEY;
+  cipher = crypto.createCipher('aes192', key)
+  cipher.update(req.body.email, 'binary', 'hex')
+  encodedString = cipher.final('hex')
+  console.log("Voici le FindOne = ", encodedString)
+// Compare the encrypted email with the one in the database
+User.findOne({ email: encodedString })
+  .then(user => {
+    if (!user) {
+      return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+    }
+   // Bcrypt compares the hash of the password here
+    bcrypt.compare(req.body.password, user.password)
+      .then(valid => {
+        if (!valid) {
+          return res.status(401).json({ error: 'Mot de passe incorrect !' });
+        }
+        res.status(200).json({
+          userId: user._id,
+          token: jwt.sign(
+            // Authentification Token for 24h
+            { userId: user._id },
+            process.env.KEYTOKEN,
+            { expiresIn: '24h' }
+          )
+        });
+      })
+      .catch(error => res.status(500).json({ error }));
+  })
+  .catch(error => res.status(500).json({ error }));
 };
